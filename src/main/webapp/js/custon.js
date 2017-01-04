01665608d17f39ebc3850531e129e65206040d94
sgururajshetty <sgururajshetty@gmail.com>
2017-01-04 16:26:18 +0530

    Revert "Document Changes and Optimization"
    This reverts commit 65061b9a3b82596bd9ad97f60dfc111ac234011b.

--- src/main/webapp/js/custon.js
+++ src/main/webapp/js/custon.js
@@ -1,34 +1,8 @@
-/*
-$(window).resize(function(){
-	if ($(window).width() < 767){		
-		 
-			$('ul.nav li.dropdown').hover(function() {
-			  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(500);
-			}, function() {
-			  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(500);
-			});
-	}	
-});*/
 
-$(document).ready(function(){
-		if ($(window).width() > 767){		
-			$('ul.nav li.dropdown').hover(function() {
-					  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(500);
-					}, function() {
-					  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(500);
-			});
-          }
-});	
+/// Drop down style  ///
 
-
-
-//* Mobile nav//
-/*
-   $(document).ready(function() {
-        $('.leftnav').click(function() {
-                $('.list-group').slideToggle(500);
-        });
-    });
-*/
- 
-
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(500);
+});
