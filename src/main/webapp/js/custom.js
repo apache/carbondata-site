d41430e2afbda77223d4cdab296989135a7681fc
jatin <jatin.demla@knoldus.in>
2017-04-18 14:08:13 +0530

    Updated apache logo and fixed minor ui issues

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -72,3 +72,17 @@ $(document).ready(function(){
       });
   });
 
+//headings scroll in top
+$(document).ready(function() {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+         if (location.pathname.replace(/^\/ /, '') == this.pathname.replace(/^\/ /, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({scrollTop: target.offset().top - 56}, 1000);
+                return false;
+            }
+        }
+    });
+});
+
