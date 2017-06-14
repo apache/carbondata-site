2ab45367b66974139f8602337997be6fb12650b6
kamleshknoldus <kamlesh@knoldus.in>
2017-06-14 18:26:59 +0530

    fixed issues for search,heading scroller,errorpage and home page minor issues

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -3,78 +3,29 @@
 /*-----------------------------------------------------------*/
 $(document).ready(function(){
 
-		if ($(window).width() > 767){		
+		if ($(window).width() > 767){
 			$('ul.nav li.dropdown').hover(function() {
 					  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(500);
 					}, function() {
 					  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(500);
 			});
     }
-});	
+});
 
 
-// Documentation page perticular links from landing page
-/*-----------------------------------------------------------*/
-
-/*function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}*/
-
-// conditional loading of documentation page from landing page
-$(document).ready(function(){
-   var pagelink;
-   var pageLoad = getParameterByName("page", window.location.href);
-   console.log(pageLoad);
-    switch(pageLoad) {
-
-    case "userguide":
-      pagelink ="user-guide-toc.html"
-      break;
-
-    case "uniqueData":
-     pagelink ="overview-of-carbondata.html"
-     break;
-
-    case "quickStart":
-      pagelink ="quick-start-guide.html"
-      break;
-
-    case "dml":
-      pagelink ="dml-operation-on-carbondata.html"
-      break;
-
-    case "ddl":
-      pagelink ="ddl-operation-on-carbondata.html"
-      break;
-
-    case "dm":
-      pagelink ="data-management.html"
-      break;
-
-      }
-    $("#viewpage").load(pagelink);
-
- });
-
-   /*Search box*/
-
-   $(function() {
+/*Search box*/
+  $(document).ready(function() {
       $( "#search-icon" ).click(function() {
           $( "#search-box" ).toggle();
       });
   });
 
+
+
+
 //headings scroll in top
 $(document).ready(function() {
-    $('a[href*="#"]:not([href="#"])').click(function () {
+    $('#viewpage a[href*="#"]:not([href="#"])').click(function () {
          if (location.pathname.replace(/^\/ /, '') == this.pathname.replace(/^\/ /, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -86,3 +37,11 @@ $(document).ready(function() {
     });
 });
 
+
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('.scroll-top').fadeIn();
+    } else {
+        $('.scroll-top').fadeOut();
+    }
+});
