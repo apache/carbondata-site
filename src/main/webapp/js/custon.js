15a9590d1236eee389e1c1eb972d81a152a7afbe
chenliang613 <chenliang613@apache.org>
2021-06-30 11:33:52 +0800

    remove old folder

--- src/main/webapp/js/custon.js
+++
@@ -1,8 +0,0 @@
-
-/// Drop down style  ///
-
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(500);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(500);
-});
